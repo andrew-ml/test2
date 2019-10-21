b4d4f63f9458770bd2064b2f27b910d38bab1788
Andrey Malinyak <andrey.malyniak@gmail.com>
2019-10-21 20:33:08 +0300

    build

---
+++ build/precache-manifest.e229d4d08753472ed724be9b47730f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6451ce4d5a7eba7df790ffe948e3c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ee2c2e5583ddeb703a0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "40dfdc9bfb885b7c112b",
+    "url": "/static/js/2.bb94e0cd.chunk.js"
+  },
+  {
+    "revision": "0ee2c2e5583ddeb703a0",
+    "url": "/static/js/main.b7111b7e.chunk.js"
+  },
+  {
+    "revision": "0b31ee5b29cafc647dc8",
+    "url": "/static/js/runtime-main.90312ed1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
